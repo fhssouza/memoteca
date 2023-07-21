34db0d0a524701450d6ce68a8c9241438402e550
Fábio Souza <fhssouza@gmail.com>
2023-07-21 15:05:19 -0300

    configuração de rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'listarpensamento' , pathMatch: 'full'},
+  { path: 'criarpensamento', component: CriarPensamentoComponent },
+  { path: 'listarpensamento', component: ListarPensamentoComponent },
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
