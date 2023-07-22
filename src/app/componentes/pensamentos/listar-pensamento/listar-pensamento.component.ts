518285a62a5e7ee2172d00ac267945bb32a113c2
Fábio Souza <fhssouza@gmail.com>
2023-07-22 16:39:13 -0300

    Comunicação entre os componentes

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [];
-  
+  listaPensamentos = [
+    {
+      conteudo: 'Pass informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @input()',
+      autoria: 'componete Filho',
+      modelo: 'modelo2'
+    }
+  ];
+
   constructor(){}
 
   ngOnInit(): void {
