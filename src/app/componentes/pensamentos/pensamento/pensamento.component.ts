9622d81e9cb5124f1cc837d1f62c1bda2146f662
Fábio Souza <fhssouza@gmail.com>
2023-07-24 13:57:20 -0300

    Diretiva NgClass

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -18,4 +18,11 @@ export class PensamentoComponent implements OnInit {
 
   }
 
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
 }
